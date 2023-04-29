4a80650b2b1c78e92d9d9cc897d24c6bf2ff10d1
Kaja <kathleengracenoble@gmail.com>
2023-04-28 21:57:28 -0400

    local databasee connected. can post data from addworkout to database. added validation of user requests for posting workouts

---
+++ src/database.py
@@ -0,0 +1,62 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Sequence,
+    ForeignKey,
+    Date,
+    Boolean,
+    create_engine,
+)
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.dialects.postgresql import JSON
+
+engine = create_engine("postgresql://katcha@localhost:5432/erg_track", echo=False)
+Session = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+
+class UserTable(Base):
+    __tablename__ = "user"
+
+    user_id = Column(Integer, Sequence("user_id_sequ"), primary_key=True)
+    auth_uid = Column(String)  # from firebase
+    user_name = Column(String)  # from firebase or edited by user
+    email = Column(String)  # from firebase
+
+    def __repr__(self):
+        return (
+            "<UserTable(user_id='%s', auth_uid='%s',  user_name='%s', email='%s')>"
+            % (self.user_id, self.auth_uid, self.user_name, self.email)
+        )
+
+
+class WorkoutLogTable(Base):
+    __tablename__ = "workout_log"
+
+    workout_id = Column(Integer, Sequence("workout_id_sequ"), primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.user_id"))
+    date = Column(Date)
+    time = Column(String)
+    meter = Column(Integer)
+    split = Column(String)
+    stroke_rate = Column(Integer)
+    interval = Column(Boolean)
+    image_hash = Column(String)
+    subworkouts = Column(JSON)
+
+
+Base.metadata.create_all(engine)
+
+
+# class SubWorkoutTable(Base):
+#     __tablename__ = "subworkout"
+
+#     sub_id = Column(Integer, Sequence("subworkout_id_sequ"), primary_key=True)
+#     workout_id = Column(Integer, ForeignKey("workout_log.workout_id"))
+#     sub_time = Column(String)
+#     sub_meter = Column(Integer)
+#     sub_split = Column(String)
+#     sub_stroke_rate = Column(Integer)
+#     rest = Column(String)
