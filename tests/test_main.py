cba14ca0e03f4f386872e1faea60218b8abdb894
ncernek <ncernek@gmail.com>
2023-10-19 21:05:53 -0400

    complete adding one test per endpoint

--- tests/test_main.py
+++ tests/test_main.py
@@ -40,9 +40,15 @@ def test_patch_user(client, headers):
     patch_user = PatchUserSchema(**resp.json()["body"]).todict()
     resp = client.patch("/user", headers=headers, json=patch_user)
     assert resp.json()["status_code"] == 200
-    
-def test_create_extract_and_process_ergImage(client):
-    pass
+
+
+def test_create_extract_and_process_ergImage(client, headers):
+    with open("./tests/erg-screen.jpeg", "rb") as f:
+        files = {"photo1": ("erg-screen.jpeg", f, "image/jpeg")}
+        with patch("src.main.upload_blob"):
+            resp = client.post("/ergImage", headers=headers, files=files)
+    assert "photo_hash" in resp.json()["body"]
+
 
 def test_create_workout(client, headers):
     # TODO gotta generate data for the workout
@@ -55,48 +61,56 @@ def test_read_workout(client, headers):
     resp = client.get("/workout", headers=headers)
     assert "workouts" in resp.json()["body"]
 
+
 def test_delete_workout(client, headers):
     resp = client.get("/workout", headers=headers)
     workout_id = resp.json()["body"]["workouts"][0]["workout_id"]
     resp = client.delete(f"/workout/{workout_id}", headers=headers)
     assert resp.json()["status_code"] == 200
-    
+
+
 def test_write_team(client, headers):
     post_team_data = PostTeamDataSchema(teamName="fake-team2", teamCode="fake-code").dict()
     resp = client.post("/team", headers=headers, json=post_team_data)
     assert resp.json()["status_code"] in (200, 403)
-    
+
+
 def test_read_team(client, headers):
     resp = client.get("/team", headers=headers)
     assert "team_member" in resp.json()["body"]
-    
+
+
 def test_update_team(client, headers):
     resp = client.get("/team", headers=headers)
     team_id = resp.json()["body"]["team_info"]["team_id"]
     post_team_data = PostTeamDataSchema(teamName="fake-team3", teamCode="fake-code").dict()
     resp = client.put(f"/team/{team_id}", headers=headers, json=post_team_data)
 
+
 def test_write_join_team(client, headers):
     post_team_data = PostTeamDataSchema(teamName="fake-team3", teamCode="fake-code").dict()
     resp = client.patch(f"/jointeam", headers=headers, json=post_team_data)
     assert resp.json()["status_code"] == 200
-    
+
+
 def test_read_teamlog(client, headers):
     resp = client.get("/teamlog", headers=headers)
     assert "team_workouts" in resp.json()["body"]
-    
+
+
 def test_read_team_info(client, headers):
     resp = client.get("/teamadmin", headers=headers)
     assert "team_info" in resp.json()["body"]
-    
+
+
 def test_update_admin(client, headers):
     resp = client.get("/user", headers=headers)
     new_admin_id = resp.json()["body"]["user_id"]
     resp = client.patch(f"/transferadmin/{new_admin_id}", headers=headers)
     assert resp.json()["status_code"] == 200
-    
+
+
 def test_create_feedback(client, headers):
     post_feedback = PostFeedbackSchema(**{"feedbackCategory": "fake-category", "comment": "fake-comment"}).dict()
     resp = client.post("/feedback", headers=headers, json=post_feedback)
     assert resp.json()["status_code"] == 200
-    
